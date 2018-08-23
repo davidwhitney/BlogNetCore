4de51ece75d4e69f3d3e975ad802cc25d43507a1
David Whitney <david@davidwhitney.co.uk>
2018-08-23 14:17:28 +0100

    wip

---
+++ src/BlogNetStandard/ContentConfiguration.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BlogNetStandard
+{
+    public class ContentConfiguration
+    {
+    }
+
+    public class Post
+    {
+        public string Id { get; set; }
+        public string Slug { get; set; }
+        public string Title { get; set; }
+        public string Body { get; set; }
+        public AuthorRef Author { get; set; }
+        public DateTime PublishDateUtc { get; set; }
+        public bool Published { get; set; }
+        public List<AuditHistory> Audit { get; set; }
+    }
+
+    public class AuditHistory
+    {
+    }
+
+    public class AuthorRef
+    {
+    }
+
+    public class Author
+    {
+        public List<Role> Roles { get; set; }
+    }
+
+    public enum Role
+    {
+    }
+
+    public class ContentBucket
+    {
+        public string Name { get; set; }
+    }
+}
---
+++ test/BlogNetStandard.Test.Unit/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace BlogNetStandard.Test.Unit
+{
+    public class Class1
+    {
+    }
+}
