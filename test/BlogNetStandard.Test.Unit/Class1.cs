0de6f74fd035d3892f2e6b5ed33b98f6b7abf590
David Whitney <david@davidwhitney.co.uk>
2018-08-23 15:36:20 +0100

    added test adapters

--- test/BlogNetStandard.Test.Unit/Class1.cs
+++ test/BlogNetStandard.Test.Unit/Class1.cs
@@ -1,8 +1,15 @@
-﻿using System;
+﻿using NUnit.Framework;
+using System;
 
 namespace BlogNetStandard.Test.Unit
 {
+    [TestFixture]
     public class Class1
     {
+        [Test]
+        public void True()
+        {
+            Assert.Pass();
+        }
     }
 }
